adb3e9c849d280fc1aeef34c7c706ba6acc02390
ruizfrontend <ruizfrontend@gmail.com>
2017-08-06 18:22:13 +0000

    prepano plantilals para slide de aliados

--- home-blocks/aliados.php
+++ home-blocks/aliados.php
@@ -8,18 +8,28 @@
 			<?php switch($idx): 
 				case 0: // financiación ?>
 					<div class="bl-financiacion clearfix">
-						<?php foreach ($projData['homeData']['fundaciones'] as $fundacion): ?>
-							<?php if($fundacion['url']): ?>
-								<div class="blck-logo">
-									<a target="_blank" href="<?php echo $fundacion['url']; ?>" title="<?php echo $fundacion['nombre']; ?>">
+						<?php foreach ($projData['homeData']['fundaciones'] as $idx => $fundacion): ?>
+							
+							<?php if (count($projData['homeData']['fundaciones']) > 6 && $idx % 6 == 0) :?>
+								<div class="bl-financiacion-slide bl-financiacion-slide-<?php echo intval($idx / 6); ?> clearfix">
+							<?php endif; ?>
+
+								<?php if($fundacion['url']): ?>
+									<div class="blck-logo">
+										<a target="_blank" href="<?php echo $fundacion['url']; ?>" title="<?php echo $fundacion['nombre']; ?>">
+											<img src="<?php echo $fundacion['logo']['url']; ?>" width="<?php echo $fundacion['logo']['width']; ?>" height="<?php echo $fundacion['logo']['height']; ?>" alt="<?php echo $fundacion['logo']['alt']; ?>" title="<?php echo $fundacion['logo']['title']; ?>">
+										</a>
+									</div>
+								<?php else: ?>
+									<div class="blck-logo">
 										<img src="<?php echo $fundacion['logo']['url']; ?>" width="<?php echo $fundacion['logo']['width']; ?>" height="<?php echo $fundacion['logo']['height']; ?>" alt="<?php echo $fundacion['logo']['alt']; ?>" title="<?php echo $fundacion['logo']['title']; ?>">
-									</a>
-								</div>
-							<?php else: ?>
-								<div class="blck-logo">
-									<img src="<?php echo $fundacion['logo']['url']; ?>" width="<?php echo $fundacion['logo']['width']; ?>" height="<?php echo $fundacion['logo']['height']; ?>" alt="<?php echo $fundacion['logo']['alt']; ?>" title="<?php echo $fundacion['logo']['title']; ?>">
+									</div>
+								<?php endif; ?>
+
+							<?php if (count($projData['homeData']['fundaciones']) > 6 && ($idx % 6 == 5 || $idx == count($projData['homeData']['medios']) - 1)) :?>
 								</div>
 							<?php endif; ?>
+
 						<?php endforeach; ?>
 					</div>
 				<?php break; ?>
@@ -30,18 +40,29 @@
 
 				<?php case 2: // publicación ?>
 					<div class="bl-financiacion clearfix">
-						<?php foreach ($projData['homeData']['medios'] as $fundacion): ?>
-							<?php if($fundacion['url']): ?>
-								<div class="blck-logo">
-									<a target="_blank" href="<?php echo $fundacion['url']; ?>" title="<?php echo $fundacion['nombre']; ?>">
-										<img style="display: block; margin: 0 auto 1em auto;" src="<?php echo $fundacion['logo']['url']; ?>" width="<?php echo $fundacion['logo']['width']; ?>" height="<?php echo $fundacion['logo']['height']; ?>" alt="<?php echo $fundacion['logo']['alt']; ?>" title="<?php echo $fundacion['logo']['title']; ?>">
-									</a>
-								</div>
-							<?php else: ?>
-								<div class="blck-logo">
-									<img src="<?php echo $fundacion['logo']['url']; ?>" width="<?php echo $fundacion['logo']['width']; ?>" height="<?php echo $fundacion['logo']['height']; ?>" alt="<?php echo $fundacion['logo']['alt']; ?>" title="<?php echo $fundacion['logo']['title']; ?>">
+						<?php foreach ($projData['homeData']['medios'] as $idx => $fundacion): ?>
+							
+							<?php if (count($projData['homeData']['medios']) >= 6 && $idx % 6 == 0) :?>
+								<div class="bl-financiacion-slide bl-financiacion-slide-<?php echo intval($idx / 6); ?> clearfix">
+							<?php endif; ?>
+
+								<?php if($fundacion['url']): ?>
+									<div class="blck-logo">
+										<a target="_blank" href="<?php echo $fundacion['url']; ?>" title="<?php echo $fundacion['nombre']; ?>">
+											<img style="display: block; margin: 0 auto 1em auto;" src="<?php echo $fundacion['logo']['url']; ?>" width="<?php echo $fundacion['logo']['width']; ?>" height="<?php echo $fundacion['logo']['height']; ?>" alt="<?php echo $fundacion['logo']['alt']; ?>" title="<?php echo $fundacion['logo']['title']; ?>">
+										</a>
+									</div>
+								<?php else: ?>
+
+									<div class="blck-logo">
+										<img src="<?php echo $fundacion['logo']['url']; ?>" width="<?php echo $fundacion['logo']['width']; ?>" height="<?php echo $fundacion['logo']['height']; ?>" alt="<?php echo $fundacion['logo']['alt']; ?>" title="<?php echo $fundacion['logo']['title']; ?>">
+									</div>
+								<?php endif; ?>
+
+							<?php if (count($projData['homeData']['medios']) >= 6 && ($idx % 6 == 5 || $idx == count($projData['homeData']['medios']) - 1)) :?>
 								</div>
 							<?php endif; ?>
+
 						<?php endforeach; ?>
 					</div>
 				<?php break; ?>
